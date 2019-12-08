507ef3d73e4775265509ccd2b40389314967f425
JavierPogi <57490018+JavierPogi@users.noreply.github.com>
2019-12-08 13:19:13 +0800

    Add files via upload

---
+++ MachProb#3(PY).py
@@ -0,0 +1,34 @@
+import numpy as np
+
+x = input("Input all your x-values: ").split(" ")
+y = input("Input all your y-values: ").split(" ")
+#Will take an infinite amount of inputs, seperated by spaces
+#Will turn the inputted values, into a list
+x = np.array(x,dtype=float)
+y = np.array(y,dtype=float)
+#Turns the list of inputs into an nx1 array
+if len(x) >= 10:
+    l = 10
+    for i in range(1,10):
+        pol = np.polyfit(x,y,i)
+        val = np.polyval(pol,x)
+        nor = np.linalg.norm(y-val)
+        z=nor.argmin()
+        coeff = np.polyfit(x,y,z)
+        print("Your coefficients are: \n")
+        print(coeff)
+       
+else:
+    l = len(x) - 1
+
+    for i in range(1,l):
+        pol = np.polyfit(x,y,i)
+        val = np.polyval(pol,x)
+        nor = np.linalg.norm(y-val)
+        z=nor.argmin()
+        coeff = np.polyfit(x,y,z)
+        print("Your coefficients are: \n")
+        print(coeff)
+
+
+    
